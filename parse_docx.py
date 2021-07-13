18250bbb98e0a0be682dcc0bb5c11b03827f3dd6
dooyounggo <dooyounggo@gmail.com>
2021-07-13 18:31:26 +0900

    Update parse_docx.py.

--- parse_docx.py
+++ parse_docx.py
@@ -1,9 +1,17 @@
+# https://github.com/python-openxml/python-docx/issues/74
+
 import docx
 
 doc_style = docx.Document('style.docx')
 
 paragraphs = doc_style.paragraphs
 tables = doc_style.tables
+
+for i, par in enumerate(paragraphs):
+    if par.text:
+        print(i)
+        print(par.text)
+        print(par._p.xml)
 
 for tbl in tables:
     rows = tbl.rows
