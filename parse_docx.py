bb581c496c48298c160abcda3dbfb77bce9e309e
GoDo <dooyounggo@gmail.com>
2021-07-14 20:55:36 +0900

    Update parse_docx.py.

--- parse_docx.py
+++ parse_docx.py
@@ -3,7 +3,6 @@
 import docx
 
 doc_style = docx.Document('style.docx')
-
 paragraphs = doc_style.paragraphs
 tables = doc_style.tables
 
@@ -12,10 +11,35 @@ for i, par in enumerate(paragraphs):
         print(i)
         print(par.text)
         print(par._p.xml)
+        print(docx.oxml.text.paragraph.CT_P.__dict__)
 
-for tbl in tables:
-    rows = tbl.rows
-    for i in range(len(rows)):
-        cells = tbl.row_cells(i)
-        for cell in cells:
-            print(cell.text)
+# for tbl in tables:
+#     rows = tbl.rows
+#     for i in range(len(rows)):
+#         cells = tbl.row_cells(i)
+#         for cell in cells:
+#             print(cell.text)
+
+doc_demo = docx.Document('demo.docx')
+paragraphs = doc_demo.paragraphs
+tables = doc_demo.tables
+
+for i, par in enumerate(paragraphs):
+    if par.text:
+        print(i)
+        print(par.text)
+        rPr = docx.oxml.shared.OxmlElement('w:rPr')
+        c = docx.oxml.shared.OxmlElement('w:color')
+        c.set(docx.oxml.shared.qn('w:val'), 'FF8822')
+        rPr.append(c)
+        par._p.r_lst[0].insert(0, rPr)
+        print(par._p.xml)
+
+# for tbl in tables:
+#     rows = tbl.rows
+#     for i in range(len(rows)):
+#         cells = tbl.row_cells(i)
+#         for cell in cells:
+#             print(cell.text)
+
+doc_demo.save('demo_style.docx')
