fba2020721db3c0e27393a701b64c072e8bc9453
dooyounggo <dooyounggo@gmail.com>
2021-07-22 14:20:16 +0900

    Update regbank_converter.py

--- generate_datasheet.py
+++ generate_datasheet.py
@@ -1,251 +1,260 @@
 import docx
 import numpy as np
 
+REGBANK_PATH = 'regbank.docx'
+STYLE_PATH = 'style.docx'
+OUTPUT_PATH = 'datasheet.docx'
 PREFIX = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
 TBL_WIDTHS = np.array([583, 583, 584, 583, 583, 585, 589, 585, 584, 584, 585, 584, 584, 585, 584, 585])
 
-doc_style = docx.Document('style.docx')
-doc_regbank = docx.Document('regbank.docx')
 
-par_style = doc_style.paragraphs
-tbl_style = doc_style.tables
+def generate_datasheet(regbank_path, style_path, output_path):
+    doc_style = docx.Document(style_path)
+    doc_regbank = docx.Document(regbank_path)
 
-# Clear IDs
-for par in par_style:
-    par._p.attrib.clear()
-    for r in par._p.r_lst:
-        r.attrib.clear()
-for tbl in tbl_style:
-    tbl._tbl.attrib.clear()
-    for row in tbl._tbl.tr_lst:
-        row.attrib.clear()
-        for tc in row.tc_lst:
-            tc.attrib.clear()
-            for p in tc.p_lst:
-                p.attrib.clear()
-                for r in p.r_lst:
-                    r.attrib.clear()
+    par_style = doc_style.paragraphs
+    tbl_style = doc_style.tables
 
-# Paragraphs with style
-par_name = par_style[2]._p
-for r in par_name.r_lst[2:]:
-    par_name.remove(r)
-par_blank = par_style[3]._p
-par_blank_2 = par_style[4]._p
+    # Clear IDs
+    for par in par_style:
+        par._p.attrib.clear()
+        for r in par._p.r_lst:
+            r.attrib.clear()
+    for tbl in tbl_style:
+        tbl._tbl.attrib.clear()
+        for row in tbl._tbl.tr_lst:
+            row.attrib.clear()
+            for tc in row.tc_lst:
+                tc.attrib.clear()
+                for p in tc.p_lst:
+                    p.attrib.clear()
+                    for r in p.r_lst:
+                        r.attrib.clear()
 
-# Tables with style
-tbl_overview = tbl_style[0]._tbl
-print(tbl_overview.xml)
-for row in tbl_overview.tr_lst[2:]:
-    tbl_overview.remove(row)
-for cell in tbl_overview.tr_lst[1].tc_lst:
-    for p in cell.p_lst[1:]:
-        cell.remove(p)
-    for r in cell.p_lst[0].r_lst[1:]:
-        cell.p_lst[0].remove(r)
+    # Paragraphs with style
+    par_name = par_style[2]._p
+    for r in par_name.r_lst[2:]:
+        par_name.remove(r)
+    par_blank = par_style[3]._p
+    par_blank_2 = par_style[4]._p
 
-tbl_fields = tbl_style[1]._tbl
-rows = [tbl_fields.tr_lst[1], tbl_fields.tr_lst[3]]
-for row in rows:
-    for cell in row.tc_lst[1:]:
-        row.remove(cell)
-    row.tc_lst[0].tcPr.tcW.set(docx.oxml.shared.qn('w:w'), str(sum(TBL_WIDTHS)))
-    row.tc_lst[0].tcPr.grid_span = 16
-cell_field = tbl_fields.tr_lst[1].tc_lst[0]
-cell_field.p_lst[0].r_lst[0].text = ''
-for i, gc in enumerate(tbl_fields.tblGrid.gridCol_lst):
-    TBL_WIDTHS[i] = gc.attrib[PREFIX + 'w']
+    # Tables with style
+    tbl_overview = tbl_style[0]._tbl
+    print(tbl_overview.xml)
+    for row in tbl_overview.tr_lst[2:]:
+        tbl_overview.remove(row)
+    for cell in tbl_overview.tr_lst[1].tc_lst:
+        for p in cell.p_lst[1:]:
+            cell.remove(p)
+        for r in cell.p_lst[0].r_lst[1:]:
+            cell.p_lst[0].remove(r)
 
-tbl_details = tbl_style[2]._tbl
-for row in tbl_details.tr_lst[2:]:
-    tbl_details.remove(row)
-for cell in tbl_details.tr_lst[1].tc_lst:
-    for p in cell.p_lst[1:]:
-        cell.remove(p)
-    for r in cell.p_lst[0].r_lst[1:]:
-        cell.p_lst[0].remove(r)
-row_details = tbl_details.tr_lst[1]
+    tbl_fields = tbl_style[1]._tbl
+    rows = [tbl_fields.tr_lst[1], tbl_fields.tr_lst[3]]
+    for row in rows:
+        for cell in row.tc_lst[1:]:
+            row.remove(cell)
+        row.tc_lst[0].tcPr.tcW.set(docx.oxml.shared.qn('w:w'), str(sum(TBL_WIDTHS)))
+        row.tc_lst[0].tcPr.grid_span = 16
+    cell_field = tbl_fields.tr_lst[1].tc_lst[0]
+    cell_field.p_lst[0].r_lst[0].text = ''
+    for i, gc in enumerate(tbl_fields.tblGrid.gridCol_lst):
+        TBL_WIDTHS[i] = gc.attrib[PREFIX + 'w']
 
-# Parse regbank elements
-body_regbank = doc_regbank._body._body
-registers = list()
-reg_found = False
-tbl_cnt = 0
-for i, obj in enumerate(body_regbank):
-    if isinstance(obj, docx.oxml.text.paragraph.CT_P):
-        if obj.r_lst:
-            if obj.r_lst[0].text.startswith('Register: '):
-                name = ''
-                for r in obj.r_lst:
-                    name += r.text
-                name = name.replace('Register: ', '').lstrip()
-                registers.append({'name': name, 'reset': None, 'overview': None, 'fields': None, 'details': None})
-                reg_found = True
-    if isinstance(obj, docx.oxml.table.CT_Tbl):
-        if reg_found and tbl_cnt == 0:
-            registers[-1]['overview'] = obj
-            tbl_cnt += 1
-        elif reg_found and tbl_cnt == 1:
-            tbl_cnt += 1
-            registers[-1]['fields'] = obj
-        elif reg_found and tbl_cnt == 2:
-            registers[-1]['details'] = obj
-            reg_found = False
-            tbl_cnt = 0
-reg_idx = 0
-for tbl in doc_regbank.tables:
-    text = ''
-    for r in tbl._tbl.tr_lst[0].tc_lst[0].p_lst[0].r_lst:
-        text += r.text
-    if text.startswith('Address'):
-        for row in tbl._tbl.tr_lst[1:]:
-            reset = ''
-            for r in row.tc_lst[3].p_lst[0].r_lst:
-                reset += r.text
-            registers[reg_idx]['reset'] = reset.rstrip()
-            reg_idx += 1
+    tbl_details = tbl_style[2]._tbl
+    for row in tbl_details.tr_lst[2:]:
+        tbl_details.remove(row)
+    for cell in tbl_details.tr_lst[1].tc_lst:
+        for p in cell.p_lst[1:]:
+            cell.remove(p)
+        for r in cell.p_lst[0].r_lst[1:]:
+            cell.p_lst[0].remove(r)
+    row_details = tbl_details.tr_lst[1]
 
-# Generate datasheet
-body_style = doc_style._body._body
-for reg in registers:
-    new_row = tbl_overview.tr_lst[1].__copy__()
-    new_row.tc_lst[0].p_lst[0].r_lst[0].text = reg['name'].upper()
+    # Parse regbank elements
+    body_regbank = doc_regbank._body._body
+    registers = list()
+    reg_found = False
+    tbl_cnt = 0
+    for i, obj in enumerate(body_regbank):
+        if isinstance(obj, docx.oxml.text.paragraph.CT_P):
+            if obj.r_lst:
+                if obj.r_lst[0].text.startswith('Register: '):
+                    name = ''
+                    for r in obj.r_lst:
+                        name += r.text
+                    name = name.replace('Register: ', '').lstrip()
+                    registers.append({'name': name, 'reset': None, 'overview': None, 'fields': None, 'details': None})
+                    reg_found = True
+        if isinstance(obj, docx.oxml.table.CT_Tbl):
+            if reg_found and tbl_cnt == 0:
+                registers[-1]['overview'] = obj
+                tbl_cnt += 1
+            elif reg_found and tbl_cnt == 1:
+                tbl_cnt += 1
+                registers[-1]['fields'] = obj
+            elif reg_found and tbl_cnt == 2:
+                registers[-1]['details'] = obj
+                reg_found = False
+                tbl_cnt = 0
+    reg_idx = 0
+    for tbl in doc_regbank.tables:
+        text = ''
+        for r in tbl._tbl.tr_lst[0].tc_lst[0].p_lst[0].r_lst:
+            text += r.text
+        if text.startswith('Address'):
+            for row in tbl._tbl.tr_lst[1:]:
+                reset = ''
+                for r in row.tc_lst[3].p_lst[0].r_lst:
+                    reset += r.text
+                registers[reg_idx]['reset'] = reset.rstrip()
+                reg_idx += 1
 
-    addr = ''
-    for r in reg['overview'].tr_lst[2].tc_lst[1].p_lst[0].r_lst:
-        addr += r.text
-    new_row.tc_lst[1].p_lst[0].r_lst[0].text = addr.lstrip(': ')
+    # Generate datasheet
+    body_style = doc_style._body._body
+    for reg in registers:
+        new_row = tbl_overview.tr_lst[1].__copy__()
+        new_row.tc_lst[0].p_lst[0].r_lst[0].text = reg['name'].upper()
 
-    access = ''
-    for row in reg['details'].tr_lst[1:]:
-        for p in row.tc_lst[3].p_lst:
-            access += p.r_lst[0].text
-    if 'R' in access and 'W' in access:
-        new_row.tc_lst[2].p_lst[0].r_lst[0].text = 'RW'
-    elif 'RO' in access:
-        new_row.tc_lst[2].p_lst[0].r_lst[0].text = 'RO'
-    elif 'WO' in access:
-        new_row.tc_lst[2].p_lst[0].r_lst[0].text = 'WO'
-
-    new_row.tc_lst[3].p_lst[0].r_lst[0].text = reg['reset'].replace(' ', '_')
-
-    desc = ''
-    for r in reg['overview'].tr_lst[0].tc_lst[1].p_lst[0].r_lst:
-        desc += r.text
-    new_row.tc_lst[4].p_lst[0].r_lst[0].text = desc.lstrip(': ')
-
-    tbl_overview.append(new_row)
-
-    new_name = par_name.__copy__()
-    new_name.r_lst[0].text = reg['name'].upper()
-    new_name.r_lst[1].text = '\t' + addr.lstrip(': ')
-    body_style.append(new_name)
-
-    new_tbl = tbl_fields.__copy__()
-    rows_reg = [reg['fields'].tr_lst[1], reg['fields'].tr_lst[4]]
-    rows_new = [new_tbl.tr_lst[1], new_tbl.tr_lst[3]]
-    for row_reg, row_new in zip(rows_reg, rows_new):
-        i = 0
-        for cell in row_reg.tc_lst:
-            gs = cell.tcPr.grid_span
-            text = ''
-            for r in cell.p_lst[0].r_lst:
-                text += r.text
-            if text:
-                if i == 0:
-                    new_cell = row_new.tc_lst[0]
-                else:
-                    new_cell = cell_field.__copy__()
-                new_cell.tcPr.grid_span = gs
-                new_cell.tcPr.tcW.set(docx.oxml.shared.qn('w:w'), str(sum(TBL_WIDTHS[i:i + gs])))
-                i += gs
-                new_run = docx.oxml.shared.OxmlElement('w:r')
-                new_run.text = text
-                new_cell.p_lst[0]._insert_r(new_run)
-                if i > 0:
-                    row_new.append(new_cell)
-            else:
-                # for j in range(i, i + gs):
-                #     if j == 0:
-                #         new_cell = row_new.tc_lst[0]
-                #     else:
-                #         new_cell = cell_field.__copy__()
-                #     new_cell.tcPr.grid_span = 1
-                #     new_cell.tcPr.tcW.set(docx.oxml.shared.qn('w:w'), str(TBL_WIDTHS[j]))
-                #     if i > 0:
-                #         row_new.append(new_cell)
-                #     i += 1
-                if i == 0:
-                    new_cell = row_new.tc_lst[0]
-                else:
-                    new_cell = cell_field.__copy__()
-                new_cell.tcPr.grid_span = gs
-                new_cell.tcPr.tcW.set(docx.oxml.shared.qn('w:w'), str(sum(TBL_WIDTHS[i:i + gs])))
-                i += gs
-                if i > 0:
-                    row_new.append(new_cell)
-
-    body_style.append(new_tbl)
-    body_style.append(par_blank.__copy__())
-
-    new_tbl = tbl_details.__copy__()
-    rows = reg['details'].tr_lst[1:]
-    for i, row in enumerate(rows):
-        if i == 0:
-            new_row = new_tbl.tr_lst[1]
-        else:
-            new_row = row_details.__copy__()
-
-        bits = ''
-        for r in row.tc_lst[0].p_lst[0].r_lst:
-            bits += r.text
-        # if ':' not in bits:
-        #     bits = bits + ':' + bits
-        new_row.tc_lst[0].p_lst[0].r_lst[0].text = bits.rstrip()
-
-        name = ''
-        for p in row.tc_lst[1].p_lst:
-            for r in p.r_lst:
-                name += r.text
-        new_row.tc_lst[1].p_lst[0].r_lst[0].text = name.rstrip()
+        addr = ''
+        for r in reg['overview'].tr_lst[2].tc_lst[1].p_lst[0].r_lst:
+            addr += r.text
+        new_row.tc_lst[1].p_lst[0].r_lst[0].text = addr.lstrip(': ')
 
         access = ''
-        for j, p in enumerate(row.tc_lst[3].p_lst):
-            text = ''
-            for r in p.r_lst:
-                text += r.text
-            if '-' not in text:
-                if j > 0:
-                    access += '\n'
-                access += text.rstrip()
-        new_row.tc_lst[2].p_lst[0].r_lst[0].text = access
+        for row in reg['details'].tr_lst[1:]:
+            for p in row.tc_lst[3].p_lst:
+                access += p.r_lst[0].text
+        if 'R' in access and 'W' in access:
+            new_row.tc_lst[2].p_lst[0].r_lst[0].text = 'RW'
+        elif 'RO' in access:
+            new_row.tc_lst[2].p_lst[0].r_lst[0].text = 'RO'
+        elif 'WO' in access:
+            new_row.tc_lst[2].p_lst[0].r_lst[0].text = 'WO'
 
-        reset = ''
-        for j, p in enumerate(row.tc_lst[4].p_lst):
-            if j > 0:
-                reset += '_'
-            for r in p.r_lst:
-                reset += r.text
-        new_row.tc_lst[3].p_lst[0].r_lst[0].text = reset.rstrip().replace(' ', '_')
+        new_row.tc_lst[3].p_lst[0].r_lst[0].text = reg['reset'].replace(' ', '_')
 
         desc = ''
-        for j, p in enumerate(row.tc_lst[2].p_lst):
-            if j > 0:
-                desc += '\n'
-            for r in p.r_lst:
-                desc += r.text
-        new_row.tc_lst[4].p_lst[0].r_lst[0].text = desc.rstrip()
+        for r in reg['overview'].tr_lst[0].tc_lst[1].p_lst[0].r_lst:
+            desc += r.text
+        new_row.tc_lst[4].p_lst[0].r_lst[0].text = desc.lstrip(': ')
 
-        if i > 0:
-            new_tbl.append(new_row)
-    body_style.append(new_tbl)
-    body_style.append(par_blank.__copy__())
+        tbl_overview.append(new_row)
 
-tbl_overview.remove(tbl_overview.tr_lst[1])
-body_style.remove(par_name)
-body_style.remove(tbl_fields)
-body_style.remove(par_blank)
-body_style.remove(tbl_details)
-body_style.remove(par_blank_2)
+        new_name = par_name.__copy__()
+        new_name.r_lst[0].text = reg['name'].upper()
+        new_name.r_lst[1].text = '\t' + addr.lstrip(': ')
+        body_style.append(new_name)
 
-doc_style.save('datasheet.docx')
+        new_tbl = tbl_fields.__copy__()
+        rows_reg = [reg['fields'].tr_lst[1], reg['fields'].tr_lst[4]]
+        rows_new = [new_tbl.tr_lst[1], new_tbl.tr_lst[3]]
+        for row_reg, row_new in zip(rows_reg, rows_new):
+            i = 0
+            for cell in row_reg.tc_lst:
+                gs = cell.tcPr.grid_span
+                text = ''
+                for r in cell.p_lst[0].r_lst:
+                    text += r.text
+                if text:
+                    if i == 0:
+                        new_cell = row_new.tc_lst[0]
+                    else:
+                        new_cell = cell_field.__copy__()
+                    new_cell.tcPr.grid_span = gs
+                    new_cell.tcPr.tcW.set(docx.oxml.shared.qn('w:w'), str(sum(TBL_WIDTHS[i:i + gs])))
+                    i += gs
+                    new_run = docx.oxml.shared.OxmlElement('w:r')
+                    new_run.text = text
+                    new_cell.p_lst[0]._insert_r(new_run)
+                    if i > 0:
+                        row_new.append(new_cell)
+                else:
+                    # for j in range(i, i + gs):
+                    #     if j == 0:
+                    #         new_cell = row_new.tc_lst[0]
+                    #     else:
+                    #         new_cell = cell_field.__copy__()
+                    #     new_cell.tcPr.grid_span = 1
+                    #     new_cell.tcPr.tcW.set(docx.oxml.shared.qn('w:w'), str(TBL_WIDTHS[j]))
+                    #     if i > 0:
+                    #         row_new.append(new_cell)
+                    #     i += 1
+                    if i == 0:
+                        new_cell = row_new.tc_lst[0]
+                    else:
+                        new_cell = cell_field.__copy__()
+                    new_cell.tcPr.grid_span = gs
+                    new_cell.tcPr.tcW.set(docx.oxml.shared.qn('w:w'), str(sum(TBL_WIDTHS[i:i + gs])))
+                    i += gs
+                    if i > 0:
+                        row_new.append(new_cell)
+
+        body_style.append(new_tbl)
+        body_style.append(par_blank.__copy__())
+
+        new_tbl = tbl_details.__copy__()
+        rows = reg['details'].tr_lst[1:]
+        for i, row in enumerate(rows):
+            if i == 0:
+                new_row = new_tbl.tr_lst[1]
+            else:
+                new_row = row_details.__copy__()
+
+            bits = ''
+            for r in row.tc_lst[0].p_lst[0].r_lst:
+                bits += r.text
+            # if ':' not in bits:
+            #     bits = bits + ':' + bits
+            new_row.tc_lst[0].p_lst[0].r_lst[0].text = bits.rstrip()
+
+            name = ''
+            for p in row.tc_lst[1].p_lst:
+                for r in p.r_lst:
+                    name += r.text
+            new_row.tc_lst[1].p_lst[0].r_lst[0].text = name.rstrip()
+
+            access = ''
+            for j, p in enumerate(row.tc_lst[3].p_lst):
+                text = ''
+                for r in p.r_lst:
+                    text += r.text
+                if '-' not in text:
+                    if j > 0:
+                        access += '\n'
+                    access += text.rstrip()
+            new_row.tc_lst[2].p_lst[0].r_lst[0].text = access
+
+            reset = ''
+            for j, p in enumerate(row.tc_lst[4].p_lst):
+                if j > 0:
+                    reset += '_'
+                for r in p.r_lst:
+                    reset += r.text
+            new_row.tc_lst[3].p_lst[0].r_lst[0].text = reset.rstrip().replace(' ', '_')
+
+            desc = ''
+            for j, p in enumerate(row.tc_lst[2].p_lst):
+                if j > 0:
+                    desc += '\n'
+                for r in p.r_lst:
+                    desc += r.text
+            new_row.tc_lst[4].p_lst[0].r_lst[0].text = desc.rstrip()
+
+            if i > 0:
+                new_tbl.append(new_row)
+        body_style.append(new_tbl)
+        body_style.append(par_blank.__copy__())
+
+    tbl_overview.remove(tbl_overview.tr_lst[1])
+    body_style.remove(par_name)
+    body_style.remove(tbl_fields)
+    body_style.remove(par_blank)
+    body_style.remove(tbl_details)
+    body_style.remove(par_blank_2)
+
+    doc_style.save(output_path)
+
+
+if __name__ == '__main__':
+    generate_datasheet(REGBANK_PATH, STYLE_PATH, OUTPUT_PATH)
--- regbank_converter.py
+++ regbank_converter.py
@@ -6,41 +6,116 @@
 #
 # WARNING! All changes made in this file will be lost!
 
+import os
 from PyQt5 import QtCore, QtGui, QtWidgets
+from generate_datasheet import generate_datasheet
+
 
 class Ui_Dialog(object):
-    def setupUi(self, Dialog):
+    def setupUi(self, Dialog, app):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(300, 200)
+        Dialog.resize(480, 240)
         self.ConvertButton = QtWidgets.QPushButton(Dialog)
-        self.ConvertButton.setGeometry(QtCore.QRect(100, 140, 100, 40))
+        self.ConvertButton.setGeometry(QtCore.QRect(190, 110, 100, 40))
         self.ConvertButton.setObjectName("ConvertButton")
         self.RegbankButton = QtWidgets.QPushButton(Dialog)
-        self.RegbankButton.setGeometry(QtCore.QRect(55, 90, 50, 20))
+        self.RegbankButton.setGeometry(QtCore.QRect(100, 110, 50, 20))
         self.RegbankButton.setObjectName("RegbankButton")
         self.StyleButton = QtWidgets.QPushButton(Dialog)
-        self.StyleButton.setGeometry(QtCore.QRect(195, 90, 50, 20))
+        self.StyleButton.setGeometry(QtCore.QRect(330, 110, 50, 20))
         self.StyleButton.setObjectName("StyleButton")
         self.StyleLabel = QtWidgets.QLabel(Dialog)
-        self.StyleLabel.setGeometry(QtCore.QRect(160, 20, 120, 60))
+        self.StyleLabel.setGeometry(QtCore.QRect(250, 40, 210, 60))
         self.StyleLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.StyleLabel.setWordWrap(True)
         self.StyleLabel.setObjectName("StyleLabel")
         self.RegbankLabel = QtWidgets.QLabel(Dialog)
-        self.RegbankLabel.setGeometry(QtCore.QRect(20, 20, 120, 60))
+        self.RegbankLabel.setGeometry(QtCore.QRect(20, 40, 210, 60))
         self.RegbankLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.RegbankLabel.setWordWrap(True)
         self.RegbankLabel.setObjectName("RegbankLabel")
+        self.TextBrowser = QtWidgets.QTextBrowser(Dialog)
+        self.TextBrowser.setGeometry(QtCore.QRect(20, 160, 440, 70))
+        self.TextBrowser.setObjectName("TextBrowser")
+        self.RegDescLabel = QtWidgets.QLabel(Dialog)
+        self.RegDescLabel.setGeometry(QtCore.QRect(50, 10, 150, 20))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.RegDescLabel.setFont(font)
+        self.RegDescLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.RegDescLabel.setObjectName("RegDescLabel")
+        self.DataStyleLabel = QtWidgets.QLabel(Dialog)
+        self.DataStyleLabel.setGeometry(QtCore.QRect(280, 10, 150, 20))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.DataStyleLabel.setFont(font)
+        self.DataStyleLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.DataStyleLabel.setObjectName("DataStyleLabel")
+
+        self.app = app
+        self.regbank_path = 'regbank.docx'
+        self.style_path = 'style.docx'
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
+        self.RegbankButton.clicked.connect(self.regbank_button_clicked)
+        self.StyleButton.clicked.connect(self.style_button_clicked)
+        self.ConvertButton.clicked.connect(self.convert_button_clicked)
+
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        Dialog.setWindowTitle(_translate("Dialog", "Register Bank Description Converter"))
+        self.ConvertButton.setToolTip(_translate("Dialog", "Convert register bank description."))
         self.ConvertButton.setText(_translate("Dialog", "Convert"))
+        self.RegbankButton.setToolTip(_translate("Dialog", "Load Magillem-generated register description."))
         self.RegbankButton.setText(_translate("Dialog", "Load"))
+        self.StyleButton.setToolTip(_translate("Dialog", "Load Telechips datasheet style."))
         self.StyleButton.setText(_translate("Dialog", "Load"))
         self.StyleLabel.setText(_translate("Dialog", "style.docx"))
         self.RegbankLabel.setText(_translate("Dialog", "regbank.docx"))
+        self.RegDescLabel.setText(_translate("Dialog", "Register Description"))
+        self.DataStyleLabel.setText(_translate("Dialog", "Datasheet Style"))
+
+    def regbank_button_clicked(self):
+        fname = QtWidgets.QFileDialog.getOpenFileName(caption='Select Register Bank Description',
+                                                      filter='MS word docx (*.docx)')[0]
+        if fname:
+            self.regbank_path = fname
+            self.RegbankLabel.setText(fname)
+            self.uiprint('Register description file path: ' + fname, color=(0, 0, 0.5))
+
+    def style_button_clicked(self):
+        fname = QtWidgets.QFileDialog.getOpenFileName(caption='Select Datasheet Style',
+                                                      filter='MS word docx (*.docx)')[0]
+        if fname:
+            self.style_path = fname
+            self.StyleLabel.setText(fname)
+            self.uiprint('Datasheet style file path: ' + fname, color=(0, 0.5, 0))
+
+    def convert_button_clicked(self):
+        fname = QtWidgets.QFileDialog.getSaveFileName(caption='Choose Output Directory and Name',
+                                                      filter='MS word docx (*.docx)',
+                                                      directory='./datasheet.docx')[0]
+        if fname:
+            if not os.path.exists(self.regbank_path):
+                self.uiprint(f'Error: Register description does not exists ({self.regbank_path}).', color=(0.5, 0, 0))
+                return
+            if not os.path.exists(self.style_path):
+                self.uiprint(f'Error: Datasheet style does not exists ({self.style_path}).', color=(0.5, 0, 0))
+                return
+            generate_datasheet(self.regbank_path, self.style_path, fname)
+            self.uiprint('Conversion complete!')
+
+    def uiprint(self, string_like, color=(0, 0, 0)):
+        string = str(string_like)
+        color_code = ''.join([f'{int(min(rgb, 1.0)*255):02x}' for rgb in color])
+        colored_string = f'<span style=\" color: #{color_code};\">{string}</span>'
+        self.TextBrowser.append(colored_string)
+        self.app.processEvents()
+        print(string)
 
 
 if __name__ == "__main__":
@@ -48,7 +123,7 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     Dialog = QtWidgets.QDialog()
     ui = Ui_Dialog()
-    ui.setupUi(Dialog)
+    ui.setupUi(Dialog, app)
     Dialog.show()
     sys.exit(app.exec_())
 
